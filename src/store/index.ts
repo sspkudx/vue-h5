9de6eda47d62d58d90b4da3a9ccc9011a0cdf9ef
yueb <nobodyplay@yeah.net>
2025-08-27 09:15:42 +0800

    chore: Add documentation comments for Vue plugins, Pinia instance, and utility functions

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,10 @@
 import { createPinia } from 'pinia';
-
+/**
+ * The Pinia instance used by the app.
+ * Define your stores in the ./modules folder.
+ *
+ * See the official documentation: https://pinia.vuejs.org/
+ */
 const pinia = createPinia();
 
 export { pinia };
