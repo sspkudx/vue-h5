bfc016670637c252b923445f5011dd3f04687b99
yueb <nobodyplay@yeah.net>
2026-04-23 01:58:32 +0800

    feat: add pinia store to the example app

--- packages/example-app/src/plugins/index.ts
+++ packages/example-app/src/plugins/index.ts
@@ -1,6 +1,9 @@
-import { Plugin } from 'vue';
+import { type Plugin } from 'vue';
+import { createPinia } from 'pinia';
 import router from '@/router';
 
-const pluginsList = Object.freeze<Plugin[]>([router]);
+const store = createPinia();
+const pluginsList = Object.freeze<Plugin[]>([router, store]);
 
+export { store };
 export default pluginsList;
